35939be8d9fa0e6c6e8cb3bf1b21c691d8602c39
Mauricio Heredia <mauh99@gmail.com>
2022-03-14 08:28:18 -0300

    Add Speck Test Lib and configuration to project

--- app/src/test/java/mzx/mymarvel/ExampleUnitTest.kt
+++ app/src/test/java/mzx/mymarvel/ExampleUnitTest.kt
@@ -1,17 +1,20 @@
 package mzx.mymarvel
 
-import org.junit.Test
-
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.gherkin.Feature
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+object ExampleUnitTest: Spek( {
+    Feature("Check addition test") {
+        Scenario("Addition") {
+            Then("check sum") {
+                assertEquals(4, 2 + 2)
+            }
+        }
     }
-}
+})
